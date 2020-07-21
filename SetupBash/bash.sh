147f8162a9b54c53b5d7d76c1c029e2adfe7789d
madara-tribe <48679574+madara-tribe@users.noreply.github.com>
2020-07-21 13:40:55 +0900

    Update bash.sh

--- SetupBash/bash.sh
+++ SetupBash/bash.sh
@@ -2,4 +2,4 @@
 alias count='ls -l | wc -l'
 alias desk='cd; cd desktop'
 alias down='cd; cd downloads'
-alias cenv='Python3.7 -m venv venv && source venv/bin/activate'
+alias cenv='python3 -m venv venv && source venv/bin/activate'
