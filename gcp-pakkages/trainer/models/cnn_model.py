24ee649de536b8eb679d71592b7c0820b2080d70
madara-tribe <48679574+madara-tribe@users.noreply.github.com>
2021-03-07 21:38:39 +0900

    Update cnn_model.py

--- gcp-pakkages/trainer/models/cnn_model.py
+++ gcp-pakkages/trainer/models/cnn_model.py
@@ -14,7 +14,7 @@ def create_model(input_shape=(32,32,3), num_cls=10):
   o = models.scse.channel_spatial_squeeze_excite(o)
   o = MaxPooling2D((2, 2))(o)
   o = Conv2D(64, (3, 3), padding='same')(o)
-  o = channel_spatial_squeeze_excite(o)
+  o = models.scse.channel_spatial_squeeze_excite(o)
   o = Dense(64, activation='relu')(o)
   o = GlobalAveragePooling2D()(o)
   o = Dense(num_cls, activation='softmax')(o)
