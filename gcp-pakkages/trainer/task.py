0ff561e44f075b15a0414a0c89ad78379a632959
madara-tribe <48679574+madara-tribe@users.noreply.github.com>
2021-03-13 10:57:12 +0900

    Update task.py

--- gcp-pakkages/trainer/task.py
+++ gcp-pakkages/trainer/task.py
@@ -1,19 +1,4 @@
-# Copyright 2019 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the \"License\");
-# you may not use this file except in compliance with the License.\n",
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an \"AS IS\" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import argparse
-
 import data_utils
 import model
 
@@ -58,7 +43,7 @@ def get_args():
                         help='split size for training / testing dataset')
     parser.add_argument('--epochs',
                         type=int,
-                        default=10,
+                        default=1,
                         help='number of epochs to train (default: 10)')
     parser.add_argument('--seed',
                         type=int,
